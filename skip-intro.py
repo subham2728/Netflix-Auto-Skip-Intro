4381c1fd305e3d5194a82d2bae8e4a31eafc033d
subham2728 <subham.rouniyar27@gmail.com>
2022-04-03 14:55:01 +0545

    fixed time

--- skip-intro.py
+++ skip-intro.py
@@ -4,25 +4,24 @@ import keyboard
 from datetime import datetime
 import win32api, win32con
 
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
-print("\n----------Script Started----------")
-
-while keyboard.is_pressed('x') == False:
-    if pyautogui.locateOnScreen("skiprecap.png",confidence=0.8) != None:
-        click(1275,676)
-        print("Skipped Recap at : ",current_time)
-    elif pyautogui.locateOnScreen("skipintro.png",confidence=0.8) != None:
-        click(1275,676)
-        print("Skipped Intro at : ",current_time)
-    else:
-        continue
+if __name__ == "__main__":
+    print("\n----------Script Started----------")
+    while keyboard.is_pressed('x') == False:
+        if pyautogui.locateOnScreen("E:\\VisualStudio\\Python\\Netflix-Skip-Intro\\skiprecap.png",confidence=0.8) != None:
+            click(1275,676)
+            now = datetime.now()
+            print("Skipped Recap at : ",now.strftime("%H:%M:%S"))
+        elif pyautogui.locateOnScreen("E:\\VisualStudio\\Python\\Netflix-Skip-Intro\\skipintro.png",confidence=0.5) != None:
+            click(1275,676)
+            now = datetime.now()
+            print("Skipped Intro at : ",now.strftime("%H:%M:%S"))
+        else:
+            pass
 
 
 
