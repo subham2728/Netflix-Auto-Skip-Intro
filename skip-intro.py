1972cec7b65a0cf3cf05798093f55c1df77206f2
Subham <subham.rouniyar27@gmail.com>
2022-12-18 10:46:34 +0545

    fixed a bug

--- skip-intro.py
+++ skip-intro.py
@@ -1,4 +1,3 @@
-from pyautogui import *
 import pyautogui
 import keyboard
 from datetime import datetime
@@ -7,7 +6,7 @@ import win32api, win32con
 if __name__=="__main__":
     print("\n----------Script Started----------")
     while True:
-        if pyautogui.locateOnScreen("D:\\VisualStudio\\Python\\Projects\\Netflix-Skip-Intro\\skipintro.png",confidence=0.8, grayscale=True) != None:
+        if pyautogui.locateOnScreen("D:\\VisualStudio\\Python\\Projects\\Netflix-Skip-Intro\\skip_intro.png",confidence=0.8, grayscale=True) != None:
             pyautogui.leftClick(x=1797, y=956)
             now = datetime.now()
             print("Skipped Intro at : ",now.strftime("%H:%M:%S"))
